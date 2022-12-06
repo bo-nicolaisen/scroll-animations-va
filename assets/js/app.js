6834f3729e040ffa4393ed2c0f662b05e5518dc8
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2022-12-06 07:55:58 +0100

    commit
    all the codes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,94 @@
-function scrollTrigger(selector) {
-    let myElements = document.querySelectorAll(selector)
-    myElements = Array.from(myElements)
-    myElements.forEach(el => {
-        addObserver(el)
-    })
+const numSteps = 20.0;
+
+let boxElement;
 
 
-}
-function addObserver(el) {
-    // We are creating a new IntersectionObserver instance
-    let observer = new IntersectionObserver((entries, observer) => { // This takes a callback function that receives two arguments: the elements list and the observer instance.
-        entries.forEach(entry => {
-            // `entry.isIntersecting` will be true if the element is visible
-            if (entry.isIntersecting) {
-                entry.target.classList.add('active')
-                // We are removing the observer from the element after adding the active class
-                observer.unobserve(entry.target)
-            }
-        })
+
+
+// Set things up on load
+window.addEventListener("load", (event) => {
+    scrollElements = document.querySelectorAll(".scroll-reveal");
+
+    scrollElements = Array.from(scrollElements);
+
+    scrollElements.forEach(element => {
+        createObserver(element);
     })
-    // Adding the observer to the element
-    observer.observe(el)
+
+}, false);
+
+
+
+
+// Intersection Observer creation
+
+function createObserver(element) {
+    let observer;
+
+    // use threshold list for diffrent behaviours for diffrent visibility deggree
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: buildThresholdList()
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(element);
 }
-// Example usage
-scrollTrigger('.scroll-reveal')
+
+
+
+
+// call back to handles intersects  
+
+function handleIntersect(entries, observer) {
+
+    entries.forEach((entry) => {
+
+
+        if (entry.intersectionRatio > 0.4) {
+
+            /*        console.log('in: ' + entry.target);
+                   console.log(entry.target.querySelectorAll('.fade')); */
+
+            let fadeElements = Array.from(entry.target.querySelectorAll('.fade'));
+            fadeElements.forEach(element => {
+
+                if (!element.classList.contains('fadeIn')) {
+                    console.log('fade in');
+                    element.classList.add('fadeIn');
+                    element.classList.remove('fadeOut');
+                }
+            })
+
+        }
+        else {
+            let fadeElements = Array.from(entry.target.querySelectorAll('.fade'));
+
+            fadeElements.forEach(element => {
+
+                if (!element.classList.contains('fadeOut')) {
+                    console.log('fade out');
+                    element.classList.remove('fadeIn');
+                    element.classList.add('fadeOut');
+                }
+            })
+        }
+
+    });
+}
+
+
+
+function buildThresholdList() {
+    let thresholds = [];
+    let numSteps = 20;
+
+    for (let i = 1.0; i <= numSteps; i++) {
+        let ratio = i / numSteps;
+        thresholds.push(ratio);
+    }
+
+    thresholds.push(0);
+    return thresholds;
+}
